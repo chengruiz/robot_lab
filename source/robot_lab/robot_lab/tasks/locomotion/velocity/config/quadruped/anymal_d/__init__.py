722c2f2f82786f5304884e8bf4e16c16b05a25ce
Chengrui Zhu <jewel@zju.edu.cn>
2025-07-30 13:11:56 +0800

    feat: add play and train scripts for RL agent with CusRL

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/anymal_d/__init__.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/anymal_d/__init__.py
@@ -26,5 +26,6 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.rough_env_cfg:AnymalDRoughEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:AnymalDRoughPPORunnerCfg",
+        "cusrl_cfg_entry_point": f"{agents.__name__}.cusrl_ppo_cfg:AnymalDRoughTrainerCfg",
     },
 )
